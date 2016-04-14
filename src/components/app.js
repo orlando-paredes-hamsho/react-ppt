acf17cc0ecd90c9cd6e78a76a835d352cf58a1b4
Trinadh Cheepilla <trinadh478@gmail.com>
2016-04-14 22:05:03 +0000

    message

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   render(){
     return (
       <div>
-        Hello! I'm in src/components/app.js!
+        Hello! I'm new to coding src/components/app.js!
       </div>
     );
   }
