829ecfe722220a3db582a5115a59b49f7676aa53
Bugz <elbugzman@live.com.mx>
2016-04-19 21:17:49 +0000

    test

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ export default class App extends Component {
           <img src="https://www.nyfa.edu/student-resources/wp-content/uploads/2014/06/Landscape-Sunset.jpg" />  
           <img src="http://science-all.com/images/landscape/landscape-05.jpg" />
           <img src="http://www.technocrazed.com/wp-content/uploads/2015/12/Landscape-wallpaper-7.jpg" />
+          <p> this </p>
         </SlideShow>
     );
   }
