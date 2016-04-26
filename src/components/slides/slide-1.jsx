fd99249a5688e4cdb5daa28a0710873f461a63a4
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2016-04-26 20:23:48 +0000

    Add styles to slide-1

--- src/components/slides/slide-1.jsx
+++ src/components/slides/slide-1.jsx
@@ -9,6 +9,13 @@ const config = {
     title:{
         text:""
     },
+    legend: {
+        align: 'right',
+        verticalAlign: 'top',
+        layout: 'vertical',
+        x: 0,
+        y: 100
+    },
     xAxis: {
         gridLineWidth: 0,
         minorGridLineWidth: 0,
@@ -35,11 +42,11 @@ const config = {
         tickLength: 0
     },
     plotOptions: {
-        column: { animation: true }
+        column: { animation: true },
     },
     series: [
-        { type:'column', data: [6], color:'#fff', name:'Quality Assurance' },
-        { type:'column', data: [3], color:'#000', name:'Dev'}
+        { type:'column', data: [6], color:'#000', name:'Quality Assurance' },
+        { type:'column', data: [3], color:'#fff', name:'Dev'}
     ]
 };
 
@@ -52,8 +59,26 @@ class Slide1 extends Component {
     render(){
         
         return (
-            <div style={{'width':'50%','position':'absolute','bottom':'0','left':'0'}}>
-                <Highcharts config={config}></Highcharts>
+            <div>
+                <h3 style={{'maxWidth': '34%', 'fontSize': '6em', 'color': 'white', 'padding': '0 8%'}}>2010-13</h3>
+                <div className="main-section left">
+                    <Highcharts config={config}></Highcharts>
+                </div>
+                <div className="main-section right">
+                    <h3>Project Delivered</h3>
+                    <ul>
+                        <li>Member Stories and Poll</li>
+                        <li>Ask USAA Financial Advice Community</li>
+                        <li>Introduce New Social Communities</li>
+                        <li>Collect Social Media Profile on Mobile</li>
+                    </ul>
+                    <h3>Highlights</h3>
+                    <ul>
+                        <li>Start Social Media Development</li>
+                        <li>Development Started with 3 Member Team</li>
+                        <li>GDL Mobile Lab Inaugurated in 2012</li>
+                    </ul>
+                </div>
             </div>
         );
     }
