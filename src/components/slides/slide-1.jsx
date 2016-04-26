ab064a326318d6478762e15661202a2087d9e853
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2016-04-26 18:52:29 +0000

    Further style slide 1 char

--- src/components/slides/slide-1.jsx
+++ src/components/slides/slide-1.jsx
@@ -7,7 +7,7 @@ const config = {
         backgroundColor:"transparent"
     },
     title:{
-        text:"My Chart"
+        text:""
     },
     xAxis: {
         gridLineWidth: 0,
@@ -15,21 +15,31 @@ const config = {
         lineColor:'transparent',
         title: {
                 enabled: false
-        }
+        },
+        labels: {
+           enabled: false
+        },
+        minorTickLength: 0,
+        tickLength: 0
     },
     yAxis: {
         gridLineWidth: 0,
         minorGridLineWidth: 0,
         title: {
                 enabled: false
-        }
+        },
+        labels: {
+           enabled: false
+        },
+        minorTickLength: 0,
+        tickLength: 0
     },
     plotOptions: {
         column: { animation: true }
     },
     series: [
-        { type:'column', data: [6], color:'#fff', label:'Assurance' },
-        { type:'column', data: [3], color:'#000' }
+        { type:'column', data: [6], color:'#fff', name:'Quality Assurance' },
+        { type:'column', data: [3], color:'#000', name:'Dev'}
     ]
 };
 
@@ -42,7 +52,7 @@ class Slide1 extends Component {
     render(){
         
         return (
-            <div style={{'width':'50%'}}>
+            <div style={{'width':'50%','position':'absolute','bottom':'0','left':'0'}}>
                 <Highcharts config={config}></Highcharts>
             </div>
         );
