a4853fdff7dda2d8b71fe8aa76f2698f2140069e
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2017-03-12 21:55:47 +0000

    patch: more cleanup and added cover

--- src/app.js
+++ src/app.js
@@ -4,22 +4,16 @@ import _ from 'lodash';
 
 import SlideShow from './components/slide-show.jsx';
 import Cover from './components/slides/cover.jsx';
-import Slide4 from './components/slides/slide-4.jsx';
-import Slide5 from './components/slides/slide-5.jsx';
-import Slide6 from './components/slides/slide-6.jsx';
 
 export default class App extends Component {
   constructor(props){
-	super(props);
+		super(props);
   }
   
   render(){
 	return (
 		<SlideShow transition="horizontal" theme="orlando">
 			<Cover />
-			<Slide4 />
-			<Slide5 />
-			<Slide6 />
 		</SlideShow>
 	);
   }
